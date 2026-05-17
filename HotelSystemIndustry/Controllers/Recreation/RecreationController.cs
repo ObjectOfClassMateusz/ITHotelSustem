a0873da499fd656e39a1b73692010c2dcd2258d7
Kacper Gołowacz <kgolowacz1030@gmail.com>
2026-05-18 00:07:38 +0200

    Added recreation booking system, views, controller logic,  recreation module functionality

--- HotelSystemIndustry/Controllers/Recreation/RecreationController.cs
+++ HotelSystemIndustry/Controllers/Recreation/RecreationController.cs
@@ -1,13 +1,13 @@
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using HotelSystemIndustry.Infrastructure;
+using HotelSystemIndustry.Models;
 using HotelSystemIndustry.Models.Recreation;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace HotelSystemIndustry.Controllers.Recreation
 {
-    // [Authorize(Roles="Receptionist")] 
     public class RecreationController : Controller
     {
         private readonly HotelDbContext _context;
@@ -20,11 +20,7 @@ namespace HotelSystemIndustry.Controllers.Recreation
         [HttpGet]
         public async Task<IActionResult> Index()
         {
-            var facilities = await _context.RecreationFacilities
-                .AsNoTracking()
-                .ToListAsync();
-
-            return View(facilities);
+            return View();
         }
 
         [HttpGet]
@@ -35,7 +31,7 @@ namespace HotelSystemIndustry.Controllers.Recreation
             var facility = await _context.RecreationFacilities
                 .AsNoTracking()
                 .Include(f => f.Bookings)
-                    .ThenInclude(b => b.Guest)
+                .ThenInclude(b => b.Guest)
                 .FirstOrDefaultAsync(m => m.Id == id);
 
             if (facility == null) return NotFound();
@@ -51,35 +47,73 @@ namespace HotelSystemIndustry.Controllers.Recreation
             var facility = await _context.RecreationFacilities.FindAsync(facilityId);
             if (facility == null) return NotFound();
 
+            // do testów -> dodanie gościa
+            if (!await _context.Guests.AnyAsync())
+            {
+                _context.Guests.Add(new Guest { Id = Guid.NewGuid(), FirstName = "Jan", LastName = "Kowalski" });
+                await _context.SaveChangesAsync();
+            }
+
             ViewBag.FacilityName = facility.Name;
             ViewBag.FacilityId = facility.Id;
-            ViewBag.GuestsSelectList = new SelectList(await _context.Guests.AsNoTracking().ToListAsync(), "Id", "LastName");
+            var guestsList = await _context.Guests.AsNoTracking().Select(g => new {
+                                                                 Id = g.Id,
+                                                                 FullName = g.FirstName + " " + g.LastName}).ToListAsync();
 
-            return View(new RecreationBooking { FacilityId = facilityId.Value, Facility = facility, Guest = null! });
+            ViewBag.GuestsSelectList = new SelectList(guestsList, "Id", "FullName");
+
+            var bookingModel = new RecreationBooking
+            {
+                FacilityId = facilityId.Value,
+                Facility = facility,
+                Guest = null!,
+                StartTime = DateTime.Today.AddHours(12),
+                EndTime = DateTime.Today.AddHours(13)
+            };
+
+            return View(bookingModel);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Book([Bind("GuestId,FacilityId,StartTime,EndTime")] RecreationBooking booking)
         {
+            ModelState.Remove("Facility");
+            ModelState.Remove("Guest");
+
             if (ModelState.IsValid)
             {
-                booking.Id = Guid.NewGuid();
                 booking.Status = BookingStatus.SCHEDULED;
+                //specyfikacja daty dla postgresql
+                booking.StartTime = DateTime.SpecifyKind(booking.StartTime, DateTimeKind.Utc);
+                booking.EndTime = DateTime.SpecifyKind(booking.EndTime, DateTimeKind.Utc);
 
                 _context.RecreationBookings.Add(booking);
                 await _context.SaveChangesAsync();
 
-                return RedirectToAction(nameof(Index));
+                return RedirectToAction(nameof(FacilityList));
             }
 
-            ViewBag.GuestsSelectList = new SelectList(await _context.Guests.AsNoTracking().ToListAsync(), "Id", "LastName", booking.GuestId);
+            var facility = await _context.RecreationFacilities.FindAsync(booking.FacilityId);
+            ViewBag.FacilityName = facility?.Name;
+            ViewBag.FacilityId = booking.FacilityId;
+
+            var guestsList = await _context.Guests
+                .AsNoTracking()
+                .Select(g => new {
+                    Id = g.Id,
+                    FullName = g.FirstName + " " + g.LastName
+                })
+                .ToListAsync();
+
+            ViewBag.GuestsSelectList = new SelectList(guestsList, "Id", "FullName", booking.GuestId);
+
             return View(booking);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> CancelBooking(Guid id)
+        public async Task<IActionResult> CancelBooking(Guid id, Guid facilityId)
         {
             var booking = await _context.RecreationBookings.FindAsync(id);
 
@@ -88,7 +122,56 @@ namespace HotelSystemIndustry.Controllers.Recreation
             booking.Status = BookingStatus.CANCELLED;
             await _context.SaveChangesAsync();
 
-            return RedirectToAction(nameof(Index));
+            return RedirectToAction(nameof(BookingList), new {facilityId = facilityId});
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> FacilityList()
+        {
+            var facilities = await _context.RecreationFacilities.AsNoTracking().ToListAsync();
+            return View(facilities);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CreateFacility()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateFacility(RecreationFacility facility)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.RecreationFacilities.Add(facility);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(FacilityList));
+            }
+
+            return View(facility);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> BookingList(Guid facilityId)
+        {
+            var facility = await _context.RecreationFacilities
+                .AsNoTracking()
+                .Include(f => f.Bookings)
+                .ThenInclude(b => b.Guest)
+                .FirstOrDefaultAsync(f => f.Id == facilityId);
+
+            if (facility == null) return NotFound();
+
+            ViewBag.FacilityName = facility.Name;
+            ViewBag.FacilityId = facility.Id;
+
+            var activeBookings = facility.Bookings
+                .Where(b => b.Status == BookingStatus.SCHEDULED)
+                .OrderBy(b => b.StartTime)
+                .ToList();
+
+            return View(activeBookings);
         }
     }
 }
