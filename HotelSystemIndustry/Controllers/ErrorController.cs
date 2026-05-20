c22ba43e18a2babe37db0ce54f5387adf8d6cb44
Dominik J <dominik139j@gmail.com>
2026-05-20 20:34:54 +0200

    Fix in errorcontroller

--- HotelSystemIndustry/Controllers/ErrorController.cs
+++ HotelSystemIndustry/Controllers/ErrorController.cs
@@ -5,6 +5,7 @@ namespace HotelSystemIndustry.Controllers
     public class ErrorController : Controller
     {
         [Route("Error/404")]
+        [HttpGet]
         public IActionResult NotFound404()
         {
             return View("NotFound");
